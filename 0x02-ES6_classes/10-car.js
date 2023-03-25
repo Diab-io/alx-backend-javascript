91f719e74a1e22398c6b78a2133d96e892ad5168
Odimayo David <odimayodavid7@gmail.com>
2023-03-25 02:00:03 +0100

    Added file

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const NewCar = this.constructor || [Symbol.species];
+    return new NewCar();
+  }
+}
+
+export default Car;
