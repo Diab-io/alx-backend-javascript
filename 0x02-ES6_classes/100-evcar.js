b1e568c40929cf1e04b9d599e5e100c0bfbfb890
Odimayo David <odimayodavid7@gmail.com>
2023-03-25 02:15:17 +0100

    Updated files

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,8 +3,6 @@ import Car from './10-car';
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._motor = motor;
-    this._color = color;
     this._range = range;
   }
 
