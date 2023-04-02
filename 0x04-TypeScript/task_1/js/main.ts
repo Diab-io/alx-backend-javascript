a18ad54f49e392e803039254fea25c52a1de7a2c
Odimayo David <odimayodavid7@gmail.com>
2023-04-02 22:46:11 +0100

    Updated file

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,29 @@ interface Teacher {
     location: string,
     [key: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+class StudentClass{
+    private _firstName: string;
+    private _lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomeWork(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this._firstName;
+    }
+}
