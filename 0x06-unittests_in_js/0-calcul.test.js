b080ac9a0b3706b24b5782d0e28de21aa894b727
Odimayo David <odimayodavid7@gmail.com>
2023-08-25 17:37:44 +0100

    test 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('App', function() {
+    it('calculateNumber test', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4)
+    });
+
+    it('Test for first number rounded', () => {
+        assert.strictEqual(calculateNumber(2.5, 2), 5)
+    });
+
+    it('Test second number rounded', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5)
+    });
+
+    it('Test both numbers rounded', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5)
+    });
+
+    it('Test', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+    });
+})
