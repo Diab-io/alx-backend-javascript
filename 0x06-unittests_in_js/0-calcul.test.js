e018a6a9345ea564712f3d8e7c354437c2878ded
Odimayo David <odimayodavid7@gmail.com>
2023-08-25 17:49:09 +0100

    Updated code

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,18 +3,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('App', function() {
     it('calculateNumber test', function() {
-        assert.strictEqual(calculateNumber(1, 3), 4)
+        assert.strictEqual(calculateNumber(1, 3), 4);
     });
 
     it('Test for first number rounded', () => {
-        assert.strictEqual(calculateNumber(2.5, 2), 5)
+        assert.strictEqual(calculateNumber(2.5, 2), 5);
     });
 
     it('Test second number rounded', () => {
-        assert.strictEqual(calculateNumber(1, 3.7), 5)
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
 
     it('Test both numbers rounded', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('Test', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 })
