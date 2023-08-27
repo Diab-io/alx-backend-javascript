6420fc05e4613713e93ef216c4b887b4e9bb4c06
Odimayo David <odimayodavid7@gmail.com>
2023-08-27 23:32:14 +0100

    Removed commas

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,8 +2,8 @@ import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 const routeMap = (app) => {
-  app.get('/', AppController.getHomepage),
-  app.get('/students', StudentsController.getAllStudents),
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
   app.get('/students/:major', StudentsController.getAllStudentsByMajor);
 };
 
